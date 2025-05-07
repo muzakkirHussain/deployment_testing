530d041918feb21df746a2e4c26c391588c50609
Shaik Muzakkir Hussain <shaik.muzakkir@codoid.com>
2025-05-07 13:20:00 +0530

    CHORE: Update 2

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more Click on the Vite and
-        testing
+        testing 2
       </p>
     </>
   );
